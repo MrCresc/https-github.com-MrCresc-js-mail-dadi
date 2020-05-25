45a3ac78a51418c22c3eacc31e4bdf29f4ad76af
Crescenzo De Martino <cresckamina@gmail.com>
2020-05-25 17:30:06 +0200

    Inserito bottone per giocare ai dadi

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-// Variabili varie
+// Variabili varie email
 var databaseEmail = ["cresckamina@gmail.com","scolozzialessandro@gmail.com",
 "sainatoalessandro@gmail.com","passarochiara@gmail.com","moscanikolas@gmail.com"];
 var email = prompt("Inserisci il tuo indirizzo e-mail.");
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 var emailCorretta = false;
 var emailPresente = false;
+
+// Variabili varie Dadi
+var button = document.getElementById('button');
+var punteggioUtente
+var punteggioPc
 
 // Verifico se i caratteri messi dall'utente siano corretti per un indirizzo e-mail
 if (email.match(mailformat)) {
@@ -30,7 +35,30 @@ if (emailCorretta && emailPresente) {
   document.getElementById('email').innerHTML = "L'indirizzo e-mail inserito è corretto ma non risulta presente nel database."
   document.getElementById('email').className = "gold"
 } else {
-  alert("L'indirizzo e-mail inserito non è corretto, riprovare")
-  document.getElementById('email').innerHTML = "L'indirizzo e-mail inserito non è corretto, riprovare"
+  alert("L'indirizzo e-mail inserito non è corretto, aggiornare la pagina e riprovare")
+  document.getElementById('email').innerHTML = "L'indirizzo e-mail inserito non è corretto, aggiornare la pagina e riprovare"
   document.getElementById('email').className = "red"
 }
+
+// Mostro il tasto per giocare ai dadi
+document.getElementById('button').className = "visible"
+
+button.addEventListener('click',
+
+  function () {
+    var punteggioUtente = Math.floor(Math.random() * 6) + 1;
+    alert("Il tuo punteggio è " + punteggioUtente)
+    var punteggioPc = Math.floor(Math.random() * 6) + 1;
+    alert("Il punteggio del PC è " + punteggioPc)
+    if (punteggioUtente > punteggioPc) {
+      alert("Hai vinto che c... fortuna!")
+      document.getElementById('smile').className = "visible"
+    } else if (punteggioUtente < punteggioPc) {
+      alert("Hai perso MUHAHAHAHAHAHHAH!!!")
+      document.getElementById('cry').className = "visible"
+    } else {
+      alert("Hai pareggiato!")
+      document.getElementById('neutral').className = "visible"
+    }
+  }
+)
